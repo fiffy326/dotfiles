0f4feeb6233452a6c3a398ff3bc4c5f095271bab
Emma Casey <22841956+fiffy326@users.noreply.github.com>
2024-02-05 19:36:02 +1000

    Added truncation to lengthy titles

--- .local/bin/status-bar/spotify.sh
+++ .local/bin/status-bar/spotify.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
+max_length=45
+
+strlen() {
+	printf '%s' "$1" | wc -c
+}
+
 if [ "$(playerctl -p 'spotify,spotifyd' status)" = 'Playing' ]; then
 	artist="$(playerctl -p 'spotify,spotifyd' metadata artist)"
 	title="$(playerctl -p 'spotify,spotifyd' metadata title | sed -E 's/\s*-.*$//g')"
-	echo "󰓇 $artist - $title"
+	status="󰓇 $artist - $title"
+	if [ "$(strlen "$status")" -le "$max_length" ]; then
+		printf '%s' "$status"
+	else
+		total_words=$(printf '%s' "$status" | wc -w)
+		while [ "$(strlen "$status")" -gt $((max_length)) ]; do
+			status="$(printf '%s' "$status" | cut -d ' ' -f -"$total_words")"
+			total_words=$((total_words - 1))
+		done
+		printf '%s%s' "$status" "…"
+	fi
 fi
